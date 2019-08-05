ceb8e1ef5fc3c83f664498ba117bd5fb66359459
Rom Villarica <romvillarica@Roms-MacBook-Pro.local>
2019-08-05 23:32:57 +0900

    remove enter to switch?

--- src/DataSheet.js
+++ src/DataSheet.js
@@ -369,7 +369,7 @@ export default class DataSheet extends PureComponent {
         e.preventDefault()
         let func = this.onRevert // ESCAPE_KEY
         if (keyCode === ENTER_KEY) {
-          func = () => this.handleNavigate(e, {i: offset, j: 0})
+          //func = () => this.handleNavigate(e, {i: offset, j: 0})
         } else if (keyCode === TAB_KEY) {
           func = () => this.handleNavigate(e, {i: 0, j: offset}, true)
         }
